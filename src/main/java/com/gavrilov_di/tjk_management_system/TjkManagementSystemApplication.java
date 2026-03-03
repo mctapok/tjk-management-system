39bd9770c7763708fa1d5b6a29a8e376218ad0cd
mctapok <vektor.ct@gmail.com>
2026-03-03 11:37:12 +0300

    initial commit

---
+++ src/main/java/com/gavrilov_di/tjk_management_system/TjkManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gavrilov_di.tjk_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjkManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjkManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gavrilov_di/tjk_management_system/TjkManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavrilov_di.tjk_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjkManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
