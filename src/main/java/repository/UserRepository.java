4f1135a6718fe03b70e24a55313ae118eb1e2515
mctapok <vektor.ct@gmail.com>
2026-03-13 12:27:00 +0300

    add user user-roles user-repository, user-service, impl sec config

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,23 @@
+package service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("User not found " + email));
+    }
+}
